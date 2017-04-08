ab6347f3108981005718f6860be6a9a73667ee6b
myashok <shivthefearlessforever@gmail.com>
2017-04-08 21:56:38 +0530

    added Fab andBottom navigatio Bar

--- app/src/main/java/com/example/nikhilr129/forgetitnot/MainActivity.java
+++ app/src/main/java/com/example/nikhilr129/forgetitnot/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.nikhilr129.forgetitnot;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton1;
+    BottomBar  bottomNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create and applying action on floating action Menu
+        createAndApplyActionOnFloatingActionMenu();
+
+        //create BottomBar and add Tab Click Listener
+    }
+    private void createAndApplyActionOnFloatingActionMenu() {
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton1 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
+
+        floatingActionButton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu first item clicked
+
+            }
+        });
+    }
+    private void createBottomBarAndClickListener() {
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+            }
+        });
     }
 }
